3f4415c02a60b8c6521d10a9934258f19ec7af8e
pixelpedro <pixelpedro@gmail.com>
2016-09-02 06:49:51 -0400

    cleaned up code, passing tests

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,8 +1,8 @@
 #write your code here
 def ftoc(temp)
-  ((temp.to_f - 32.0) / 1.8).round(2)
+  ((temp - 32.0) / 1.8).round(2)
 end
 
 def ctof(temp)
-  temp.to_f * 1.8 + 32.0
+  temp * 1.8 + 32.0
 end
